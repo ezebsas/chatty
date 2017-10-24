5239e21989ee07170f8a6ae90f93c88ae1f17674
Ezequiel Mollinedo <mollinedoezequiel@gmail.com>
2017-10-24 15:48:25 -0300

    Added sign in, sign up, log out and authentication

---
+++ app/Models/User.php
@@ -0,0 +1,43 @@
+<?php
+	
+namespace Chatty\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+
+
+class User extends Model implements AuthenticatableContract{
+	use Authenticatable;
+
+	protected $table = 'users';
+
+	protected $fillable = [
+		'username',
+		'email',
+		'password',
+		'first_name',
+		'last_name',
+		'location',
+	];
+
+	protected $hidden = ['password', 'remember_token'];
+
+	public function getName(){
+		if($this->first_name && $this->last_name){
+			return "{$this->first_name} {$this->last_name}";
+		}
+		if($this->first_name){
+			return $this->first_name;
+		}
+		return null;
+	}
+
+	public function getNameOrUserName(){
+		return $this->getName() ?: $this->username;
+	}
+	public function getFirstNameOrUserName(){
+		return $this->first_name() ?: $this->username;
+	}
+
+}
--- resources/views/templates/partials/navigation.blade.php
+++ resources/views/templates/partials/navigation.blade.php
@@ -1,61 +1,30 @@
-<!-- <nav class="navbar navbar-light navbar-toggleable-md" role="navigation">
-  <a class="navbar-brand" href="#">Chatty</a>
-  <div class="collapse navbar-collapse">
-   {{-- @if (auth::check()) --}}
-   <ul class="navbar-nav">
-    <li class="nav-item"> <a class="nav-link" href="#">timeline</a> </li>
-    <li class="nav-item"> <a class="nav-link" href="#">friends</a> </li>
-  </ul>
-  <form class="navbar-form navbar-toggler-left" role="search" action="#">
-    <div class="form-group">
-      <input type="text" name="query" class="form-control" placeholder="find people">
-    </div>
-    <button type="submit" class="btn btn-default">search</button>
-  </form>
-  {{-- @endif --}}
-
-  <ul class="nav navbar-nav navbar-toggler-right">
-    {{-- @if (auth::check()) --}}
-    <li><a href="#">dayle{{-- {{auth::user()->getnameorusername()}} --}}</a></li>
-    <li><a href="#">update profile</a></li>
-    <li><a href="#">sign out</a></li>
-    {{-- @else --}}
-    <li><a href="#">sign up</a></li>
-    <li><a href="#">sign in</a></li>
-    {{-- @endif --}}
-  </ul>
-</div> 
-</nav> -->
-<nav class="navbar navbar-default navbar-toggleable-md">
+<nav class="navbar navbar-default">
   <div class="container">
-
-    <a class="navbar-brand" href="#">Chatty</a>
+    <div class="navbar-header">
+      <a class="navbar-brand" href="{{ route('home') }}">Chatty</a>
+    </div>
     <div class="collapse navbar-collapse">
-      <!--@if (Auth::check()) -->
-      <ul class="navbar-nav">
-        <li class="nav-item"> <a class="nav-link" href="#">Timeline</a> </li>
-        <li class="nav-item"> <a class="nav-link" href="#">Friends</a> </li>
-      </ul>
-      <form class="navbar-form navbar-toggler-left" role="search" action="#">
-        <div class="form-group">
-          <input type="text" name="query" class="form-control" placeholder="find people">
-        </div>
-        <button type="submit" class="btn btn-default">search</button>
-      </form>
-      <!-- @endif -->
-
-     
-      <div id="navbar" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav navbar-right">
-          <!--@if (Auth::check()) -->
-          <li><a href="#">Dayle{{-- {{auth::user()->getnameorusername()}} --}}</a></li>
-          <li><a href="#">Update profile</a></li>
-          <li><a href="#">Sign out</a></li>
-          <!-- @else -->
-          <li><a href="#">Sign up</a></li>
-          <li><a href="#">Sign in</a></li>
-          <!-- @endif -->
+      @if (Auth::check())
+        <ul class="nav navbar-nav">
+          <li class="nav-item"> <a class="nav-link" href="#">Timeline</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">Friends</a> </li>
         </ul>
-      </div><!--/.nav-collapse -->
+        <form class="navbar-form navbar-left" role="search" action="#">
+          <div class="form-group">
+            <input type="text" name="query" class="form-control" placeholder="Find people">
+          </div>
+          <button type="submit" class="btn btn-default">search</button>
+        </form>
+      @endif 
+      <ul class="nav navbar-nav navbar-right">
+        @if (Auth::check()) 
+          <li><a href="#">{{ Auth::user()->getNameOrUserName()}}</a></li>
+          <li><a href="#">Update profile</a></li>
+          <li><a href="{{ route('auth.signout') }}">Sign out</a></li>
+        @else 
+          <li><a href="{{ route('auth.signup') }}">Sign up</a></li>
+          <li><a href="{{ route('auth.signin') }}">Sign in</a></li>
+        @endif 
+      </ul>
     </div>
   </nav>
