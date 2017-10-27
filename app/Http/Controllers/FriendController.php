82807db6681e5c5e0b27c8e5bb6c44fb8acfe17a
Ezequiel Mollinedo <mollinedoezequiel@gmail.com>
2017-10-27 12:12:20 -0300

    Added show friend requests

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -9,9 +9,11 @@ use Auth;
 class FriendController extends Controller{
 	public function getIndex(){
 		$friends = Auth::user()->friends();
+		$requests = Auth::user()->friendRequests();
 
 		return view('friends.index')
-			->with('friends', $friends);
+			->with('friends', $friends)
+			->with('requests', $requests);
 	}
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,8 @@ class User extends Model implements AuthenticatableContract{
 		return $this->friendsOfMine()->wherePivot('accepted', true)->get()->
 			merge($this->friendOf()->wherePivot('accepted', true)->get());
 	}
+
+	public function friendRequests(){
+		return $this->friendsOfMine()->wherePivot('accepted', false)->get();
+	}
 }
