d486e1c888c207736abc2fb4a6502fabb455c1f4
Ezequiel Mollinedo <mollinedoezequiel@gmail.com>
2017-10-30 12:48:05 -0300

    Showing the timeline page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,9 +1,13 @@
 <?php
 
 namespace Chatty\Http\Controllers;
+use Auth;
 
 class HomeController extends Controller{
 	public function index(){
+		if(Auth::check()){
+			return view('timeline.index');
+		}
 		return view('home');
 	}
 }
